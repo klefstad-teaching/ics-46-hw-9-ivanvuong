de3121b304b768c2b21e3ebe4d2d416f064aec36
Ivan <vuongi2@uci.edu>
2025-03-15 00:48:58 +0000

    a

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -2,19 +2,51 @@
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
 {
-    
+    int numVertices = G.size();
+    std::vector<int> distances(numVertices, INF);
+    vector<bool> visited(numVertices, false);
+
+    distances[source] = 0;
+    previous[source] = -1;
+    priority_queue<pair<int,int>> minHeap;
+    minHeap.push({source, 0});
+    while (!minHeap.empty())
+    {
+        int u = minHeap.top().first;
+        minHeap.pop();
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (Edge edge : G[u])
+        {
+            int v = edge.dst;
+            int weight = edge.weight;
+            if (!visited[v] && distances[u] + weight < distances[v])
+            {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({v, distances[v]});
+            }
+        }
+    }
+    return distances;
 }
 
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
 {
-
+    std::vector<int> shortest_path;
+    while (destination != -1)
+    {
+        shortest_path.insert(shortest_path.begin(), destination);
+        destination = previous[destination];
+    }
+    return shortest_path;
 }
 
-void print_path(const vector<int>& v, int total)
-{
-    for (int i : v)
+void print_path(const vector<int>& path, int total_cost) {
+    for (int i = 0; i < path.size(); i++) 
     {
-        cout << i << endl;
-        cout << "Total cost is " << total;
+        cout << path[i] << " ";
     }
+    cout << endl;
+    cout << "Total cost is " << total_cost << endl;
 }
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
-    return 0;
+    Graph G;
+    file_to_graph("../src/small.txt", G);
+    vector<int> previous(G.numVertices, -1);
+    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+
+    for (int i = 0; i < G.size(); ++i)
+    {
+        vector<int> path = extract_shortest_path(distances, previous, i);
+        print_path(path, distances[i]);
+    }
 }
